1e64e1cfe123b89dad76eaacb87207d71775b260
jharwinbarrozo <jharwinbarrozo>
2021-01-08 00:35:00 +0800

    Increase uStepping, Speed and Acceleration

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,10 @@
 #define SAMPLE_TIME        0.1   // Control loop in s
 #define RATIO              54    // Gear ratio of rotator gear box for V3.1 Satnogs
-#define MICROSTEP          8     // Set Microstep
-#define MIN_PULSE_WIDTH    20    // In microsecond for AccelStepper
-#define MAX_SPEED          3200  // In steps/s, consider the microstep, default 3200
-#define MAX_ACCELERATION   1600  // In steps/s^2, consider the microstep, default 1600
-#define SPR                1600L // Step Per Revolution, consider the microstep
+#define MICROSTEP          16    // Set Microstep, A4988 1/16 ustep MS1 H, MS2 H, MS3 H. 
+#define MIN_PULSE_WIDTH    1     // In microsecond for AccelStepper, the default for genuine A4988 is 1uS
+#define MAX_SPEED          8000  // In steps/s, consider the microstep, For 1/8 ustep it is 3200, For 1/16 8000
+#define MAX_ACCELERATION   6400  // In steps/s^2, consider the microstep, For 1/8 ustep it is 1600, For 1/16 6400
+#define SPR                3200L // Step Per Revolution, consider the microstep, For 1/8 ustep it is 1600L, For 1/16 3200L
 #define MIN_M1_ANGLE       0     // Minimum angle of azimuth
 #define MAX_M1_ANGLE       360   // Maximum angle of azimuth
 #define MIN_M2_ANGLE       0     // Minimum angle of elevation
