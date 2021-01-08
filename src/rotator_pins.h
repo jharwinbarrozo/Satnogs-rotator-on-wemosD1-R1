ce68c62965f9b0f00c284bd74aa556c8e388c29b
jharwinbarrozo <jharwinbarrozo>
2021-01-08 19:17:28 +0800

    Mapped the pins for az and el axis

--- src/rotator_pins.h
+++ src/rotator_pins.h
@@ -1,15 +1,15 @@
 #ifndef ROTATOR_PINS_H_
 #define ROTATOR_PINS_H_
 
-#define M1IN1 D4 ///< Motor 1 PWM pin
-#define M1IN2 D7  ///< Motor 1 PWM pin
+#define M1IN1 D4 ///< AZ Motor STEP
+#define M1IN2 D7  ///< AZ Motor DIR
 
-#define M2IN1 D3 ///< Motor 2 PWM pin
-#define M2IN2 D6  ///< Motor 2 PWM pin
+#define M2IN1 D3 ///< EL Motor STEP
+#define M2IN2 D6  ///< EL Motor DIR
 
-#define MOTOR_EN D8 ///< Digital output, to enable the motors
+#define MOTOR_EN D8 ///< Digital output, EN pins for both A4988 drivers
 
-#define SW1 D13 ///< Digital input, to read the status of end-stop for motor 1
-#define SW2 D2 ///< Digital input, to read the status of end-stop for motor 2
+#define SW1 D13 ///< Digital input, to read the status of end-stop for AZ motor
+#define SW2 D2 ///< Digital input, to read the status of end-stop for EL motor
 
 #endif /* ROTATOR_PINS_H_ */
